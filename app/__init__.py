2e745873be77a98551939da85b52a425fb3eaba5
Aare <aare212@hotmail.com>
2020-11-02 08:42:22 +0200

    Link to /app page

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+from flask_sqlalchemy import SQLAlchemy
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///budgets.db"
+app.config["SQLALCHEMY_ECHO"] = True
+db = SQLAlchemy(app)
+
 from app import routes
+
+db.create_all()
--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,8 @@ from app import app
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/app")
+def app_page():
+    print("***** FRONT PAGE")
+    return render_template("app/app_page.html")
