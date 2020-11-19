1adbf4c657988fbf1da8b1ff4b82720482b355ac
Aare <aare212@hotmail.com>
2020-11-19 20:07:38 +0200

    Login / logout functionalities

--- app/__init__.py
+++ app/__init__.py
@@ -7,10 +7,24 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///budgets.db"
 app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 
-from app import routes # all routes via this file
-
+from app import routes # all routes handled in this file
 from app.budgets import models
-
 from app.auth import models
+
+# login
+from app.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to use this functionality."
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 db.create_all()
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, request, redirect, url_for
+from flask_login import login_user, logout_user
 from app import app, db
 from app.budgets.models import Budget
 from app.budgets.forms import BudgetForm
@@ -56,20 +57,24 @@ def create_jan():
     return redirect(url_for("jan"))
 
 
-# login / logout
+# login
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
     if request.method == "GET":
         return render_template("auth/loginform.html", form = LoginForm())
 
     form = LoginForm(request.form)
-    # mahdolliset validoinnit
 
     user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
     if not user:
         return render_template("auth/loginform.html", form = form,
                                error = "No such username or password")
 
+    login_user(user)
+    return redirect(url_for("index"))
 
-    print("Käyttäjä " + user.name + " tunnistettiin")
+# logout
+@app.route("/auth/logout")
+def auth_logout():
+    logout_user()
     return redirect(url_for("index"))
